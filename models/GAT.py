756b4e64619fd841c16073d53e0c3c5fac5dfc6b
Khoawawa <atom08072004@gmail.com>
2025-07-23 21:47:56 +0700

    a

--- models/GAT.py
+++ models/GAT.py
@@ -21,7 +21,7 @@ class GAT_Layer(nn.Module):
     def forward(self, x, edge_index):
         
         x_embed = self.embeddings(x)
-        x_gat, (gat_edge_index, attn_weights) = self.gat_conv(x_embed, edge_index)
+        x_gat = self.gat_conv(x_embed, edge_index)
         x_cat = torch.cat([x_embed,x_gat], dim=-1)
         out = F.leaky_relu(x_gat)
         out = self.dropout(out)
