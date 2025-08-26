47dabf3e5b7a55c652780ef26ee96db4a7fcfc53
Khoawawa <atom08072004@gmail.com>
2025-08-26 11:03:46 +0800

    a

--- models/Discriminator.py
+++ models/Discriminator.py
@@ -83,7 +83,9 @@ class WGANCritic(nn.Module):
     def forward(self, spatio_temporal_features, t: torch.Tensor, lens):
         spatio_temporal_features = spatio_temporal_features.permute(1,0,2) # [batch_size, seq_len, input_dim]
         t_tensor = t.unsqueeze(-1) # [batch_size, 1, 1]
-        t_tensor = t.expand(-1,spatio_temporal_features.size(1),-1)
+        print(t_tensor.shape)
+        exit()
+        t_tensor = t.expand(,spatio_temporal_features.size(1),-1)
         
         d_input = torch.concat([spatio_temporal_features, t_tensor], dim=-1) # [batch_size, seq_len, input_dim + 1]
         
