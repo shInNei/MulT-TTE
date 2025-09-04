ae8104f62dced6e6dca87d9becda1568ebd61122
Khoawawa <atom08072004@gmail.com>
2025-09-04 15:32:41 +0700

    a

--- train/train_model.py
+++ train/train_model.py
@@ -66,14 +66,13 @@ def train_model(R_model: nn.Module,D_model: nn.Module, data_loaders: Dict[str, D
                 tqdm_loader = tqdm(data_loaders[phase],mininterval=3)
                 for step, (features, truth_data) in enumerate(tqdm_loader):
                     steps += truth_data.size(0)
-                    B = features.size(0)
                     
                     features = to_var(features, args.device)
                     lens = features['lens']
                     
                     targets.append(truth_data.numpy())
                     truth_data = to_var(truth_data, args.device)
-                    
+                    B = features.size(0)
                     if phase == 'train':
                         # why noise???? --> so can model random event
                         # TODO: work on putting the noise into the generator/ regressor. However might be optional if BERT is already installed
